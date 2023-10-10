1271ba3f05d2adede8bfa3d2ec14d17c15343735
chinedu2040 <udezechinedu2040@gmail.com>
2023-10-10 21:43:56 +0100

    Fully implemented user authentications

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
-
+const authController = require('./../controllers/authController');
 const router = express.Router();
 
 // Define routes for CRUD operations (Create, Read, Update, Delete)
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(
+    authController.protect,
+    authController.restrictTo('admin'),
+    tourController.createTour
+    );
 
 router
   .route('/:id')
   .get(tourController.getTourById)
-  .patch(tourController.updateTour);
 
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin'),
+    tourController.updateTour
+    )
+    
 // Get suggested tours based on category
 router
 .route('/suggested/:categoryId')
